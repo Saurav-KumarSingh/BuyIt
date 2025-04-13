b5b3a59096cc8509c983e31fd09f4143c8b9bf26
Saurav-KumarSingh <sauravkumarsingh093@gmail.com>
2025-04-13 20:09:41 +0530

    EditProduct Page added

---
+++ frontend/src/components/Admin/EditProductPage.jsx
@@ -0,0 +1,143 @@
+import React, { useState } from 'react'
+import { CiLight } from 'react-icons/ci';
+
+const EditProductPage = () => {
+    const [productData, setProductData] = useState({
+        name: "",
+        description: "",
+        price: 0,
+        countInStock: 0,
+        sku: "",
+        category: "",
+        brand: "",
+        sizes: [],
+        colors: [],
+        collections: "",
+        material: "",
+        gender: "",
+        images: [
+            {
+                url: "https://picsum.photos/150?random=1",
+            },
+            {
+                url: "https://picsum.photos/150?random=2",
+            },
+        ],
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setProductData((preData) => ({ ...preData, [name]: value }))
+    }
+
+    const handleImgUrl = async (e) => {
+        const file =await e.target.file[0];
+        console.log(file);
+        
+
+    }
+
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+        console.log(productData);
+        
+    }
+
+    return (
+        <div className="max-w-5xl mx-auto p-6 shadow-md rounded-md">
+            <h2 className="text-3xl font-bold mb-6">Edit Product</h2>
+            <form onSubmit={handleSubmit}>
+                <div className="mb-6">
+                    <label className="block font-semibold mb-2">Product Name</label>
+                    <input
+                        type="text"
+                        name="name"
+                        value={productData.name}
+                        onChange={handleChange}
+                        className="w-full border border-gray-300 rounded-md shadow-sm py-2 "
+                        required
+                    />
+                </div>
+                {/* Description */}
+                <div className="mb-6">
+                    <label className='block font-semibold mb-2'>Description</label>
+                    <textarea name="description" value={productData.description} onChange={handleChange} className='w-full border border-gray-300 rounded-md py-2' rows={4} required />
+                </div>
+                {/* Price */}
+                <div className="mb-6">
+                    <label className="block font-semibold mb-2">Price</label>
+                    <input
+                        type="number"
+                        name="price"
+                        value={productData.price}
+                        onChange={handleChange}
+                        className="w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:border-indigo-500"
+                    />
+                </div>
+                {/* Count in Stock  */}
+                <div className="mb-6">
+                    <label className="block font-semibold mb-2">Count in Stock</label>
+                    <input
+                        type="number"
+                        name="countInStock"
+                        value={productData.countInStock}
+                        onChange={handleChange}
+                        className="w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:border-indigo-500"
+                    />
+                </div>
+                {/* SKU  */}
+                <div className="mb-6">
+                    <label className="block font-semibold mb-2">SKU</label>
+                    <input
+                        type="text"
+                        name="sku"
+                        value={productData.sku}
+                        onChange={handleChange}
+                        className="w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:border-indigo-500"
+                    />
+                </div>
+                {/* Sizes  */}
+                <div className="mb-6">
+                    <label className="block font-semibold mb-2">Sizes (comma-separated)</label>
+                    <input
+                        type="text"
+                        name="sizes"
+                        value={productData.sizes.join(", ")}
+                        onChange={(e) => setProductData({ ...productData, sizes: e.target.value.split(",").map((size) => size.trim()) })}
+                        className="w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:border-indigo-500"
+                    />
+                </div>
+                {/* Colors */}
+                <div className="mb-6">
+                    <label className="block font-semibold mb-2">Colors (comma-separated)</label>
+                    <input
+                        type="text"
+                        name="colors"
+                        value={productData.colors.join(", ")}
+                        onChange={(e) => setProductData({ ...productData, colors: e.target.value.split(",").map((color) => color.trim()) })}
+                        className="w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:border-indigo-500"
+                    />
+                </div>
+
+                {/* Image upload */}
+                <div className="mb-6">
+                    <label className="block font-semibold mb-2">Upload Image</label>
+                    <input
+                        type="file"
+                        name="colors"
+                        onChange={handleImgUrl}
+                        className="w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:border-indigo-500"
+                    />
+                    <div className="flex gap-4 mt-4">
+                        {productData.images.map((image, index) => (<div key={index}>
+                            <img src={image.url} alt={image.altText || "Product Image"} className='w-20 h-20 object-cover rounded-md shadow-md' />
+                        </div>))}
+                    </div>
+                </div>
+                <button type='submit' className='w-full bg-green-500 text-white py-2 rounded-md hover:bg-green-600 transition-colors'>Update Product</button>
+            </form>
+        </div>
+    )
+}
+
+export default EditProductPage
