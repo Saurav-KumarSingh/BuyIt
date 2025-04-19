93eb894fb8178d52273f0c6bb8b8957f78ebc001
Saurav-KumarSingh <sauravkumarsingh093@gmail.com>
2025-04-19 13:21:39 +0530

    Checkout routes ✅

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const db=require("./config/db")
 const userRoute=require("./routes/userRoutes");
 const productRoute=require("./routes/productRoutes");
 const cartRoute=require("./routes/cartRoutes");
+const checkoutRoute=require("./routes/checkoutRoutes");
 
 const app = express();
 app.use(express.json());
@@ -25,6 +26,7 @@ app.get("/", (req, res) => {
 app.use('/api/users/',userRoute);
 app.use('/api/products/',productRoute);
 app.use('/api/cart/',cartRoute);
+app.use('/api/checkout/',checkoutRoute);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
