fa9af71efc269348e75a57a546168fca0659a0a1
Saurav-KumarSingh <sauravkumarsingh093@gmail.com>
2025-04-22 22:11:17 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,23 @@
 const express = require("express");
 const cors = require("cors");
-const dotenv=require("dotenv");
-const db=require("./config/db")
-const userRoute=require("./routes/userRoutes");
-const productRoute=require("./routes/productRoutes");
-const cartRoute=require("./routes/cartRoutes");
-const checkoutRoute=require("./routes/checkoutRoutes");
-const orderRoute=require("./routes/orderRoutes");
-const uploadRoute=require("./routes/uploadRoutes");
-const subscribeRoute=require("./routes/subscribeRoute");
-const adminRoute=require("./routes/adminRoute");
-const productadminRoute=require("./routes/productAdminRoute");
-const ordersadminRoute=require("./routes/adminOrderRoute");
+const dotenv = require("dotenv");
+const db = require("./config/db");
+const userRoute = require("./routes/userRoutes");
+const productRoute = require("./routes/productRoutes");
+const cartRoute = require("./routes/cartRoutes");
+const checkoutRoute = require("./routes/checkoutRoutes");
+const orderRoute = require("./routes/orderRoutes");
+const uploadRoute = require("./routes/uploadRoutes");
+const subscribeRoute = require("./routes/subscribeRoute");
+const adminRoute = require("./routes/adminRoute");
+const productadminRoute = require("./routes/productAdminRoute");
+const ordersadminRoute = require("./routes/adminOrderRoute");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-dotenv.config(); 
-
-const PORT = process.env.PORT || 9000;
+dotenv.config();
 
 db();
 
@@ -27,24 +25,23 @@ app.get("/", (req, res) => {
   res.send("WELCOME TO RABBIT API!");
 });
 
-// API routes 
-
-app.use('/api/users/',userRoute);
-app.use('/api/products/',productRoute);
-app.use('/api/cart/',cartRoute);
-app.use('/api/checkout/',checkoutRoute);
-app.use('/api/orders/',orderRoute);
-app.use('/api/upload/',uploadRoute);
-app.use('/api/subscribe/',subscribeRoute);
-
+// API routes
+app.use("/api/users/", userRoute);
+app.use("/api/products/", productRoute);
+app.use("/api/cart/", cartRoute);
+app.use("/api/checkout/", checkoutRoute);
+app.use("/api/orders/", orderRoute);
+app.use("/api/upload/", uploadRoute);
+app.use("/api/subscribe/", subscribeRoute);
 
 // Admin routes
-app.use('/api/admin/users/',adminRoute);
-app.use('/api/admin/products/',productadminRoute);
-app.use('/api/admin/orders/',ordersadminRoute);
+app.use("/api/admin/users/", adminRoute);
+app.use("/api/admin/products/", productadminRoute);
+app.use("/api/admin/orders/", ordersadminRoute);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+// ❌ Don't listen to a port on Vercel
+// app.listen(PORT, () => {
+//   console.log(`Server is running on http://localhost:${PORT}`);
+// });
 
 module.exports = app;
