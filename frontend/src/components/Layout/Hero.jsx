0a2579232072a379615e12702b166fc70daa6a3d
Saurav-KumarSingh <sauravkumarsingh093@gmail.com>
2025-04-23 12:03:39 +0530

    update

--- frontend/src/components/Layout/Hero.jsx
+++ frontend/src/components/Layout/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
             <div className='text-center text-white p-6'>
                 <h1 className='text-4xl md:text-9xl font-bold tracking-tighter uppercase mb-4'>Vacation <br /> Ready</h1>
                 <p className='text-sm tracking-tighter md:text-lg mb-6'>Ecplore our vecation-ready outfits with fast worldwide shipping</p>
-                <Link to="#" className="bg-white text-gray-950 px-5 py-2 rounded-sm text-lg">Shop Now</Link>
+                <Link to="collections/all" className="bg-white text-gray-950 px-5 py-2 rounded-sm text-lg">Shop Now</Link>
             </div>
         </div>
     </section>
